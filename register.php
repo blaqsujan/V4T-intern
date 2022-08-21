7e063a6a4b29e00651cec429b4fd65627aa0e175
blaqsujan <sujanmaharjan054@gmail.com>
2022-08-21 15:40:23 +0545

    initial commit

---
+++ register.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="lsection">
+        <div class="image-logo">
+            <h1>LOGO HERE</h1><br><br>
+        </div>
+        <div class="lcontent">
+            <div class="logo">
+                <h1>LOGO HERE</h1><br><br>
+            </div>
+            <div class="cname">
+                <h3><b><i>E-lectronics</i></b></h3><br><br>
+            </div>
+            <div class="slogan">
+                <p>Nepal's favorite electronic
+                    distrubuter company.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="rsection">
+        <form action="#" method="POST" enctype="multipart/form-data">
+            <div class="heading">
+                <h2>Registration Form</h2><br><br>
+            </div>
+
+            <div class="name-details">
+                <div class="field input">
+                    <label><b>Full Name</b></label>
+                    <input type="text" placeholder="Enter Full Name">
+                </div>
+
+                <div class="field input">
+                    <label><b>Email</b></label>
+                    <input type="email" placeholder="Enter First Email">
+                </div>
+
+                <div class="field input">
+                    <label><b>Password</b></label>
+                    <input type="email" placeholder="Enter First Password">
+                </div>
+
+                <div class="field image">
+                    <input type="file">
+                </div>
+
+                <div class="field button">
+                    <input type="submit" name="Signup" value="Signup">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
